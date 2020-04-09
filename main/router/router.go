4cc255bd2942c6fcf1bba5bfd6b48f63e96f39e6
ChenYuxin0721 <56210850+ChenYuxin0721@users.noreply.github.com>
2020-04-09 10:47:44 +0800

    Update router.go

--- main/router/router.go
+++ main/router/router.go
@@ -15,23 +15,23 @@ func SetupRouter()*gin.Engine{
 	v1Group := r.Group("v1")
 	{
 		//注册
-		v1Group.POST("/register",controller.Register)
+		v1Group.POST("/api/register",controller.Register)
 		//登录
-		v1Group.POST("/login",controller.Login)
+		v1Group.POST("/api/login",controller.Login)
 		//存入信息
-		v1Group.GET("/info",middleware.Middleware(),controller.Info)
+		v1Group.GET("/api/info",middleware.Middleware(),controller.Info)
 		//发表故事
-		v1Group.POST("/post", controller.CreateAStory)
+		v1Group.POST("/api/post", controller.CreateAStory)
 		//查看我的所有的故事
-		v1Group.GET("/readme", controller.ReadAllMyStory)
+		v1Group.GET("/api/readme", controller.ReadAllMyStory)
 		//查看我的某个故事
-		v1Group.GET("/readme/:id", func(c *gin.Context) {
+		v1Group.GET("/api/readme/:id", func(c *gin.Context) {
 
 		})
 		//修改我的某个故事
-		v1Group.PUT("/readme/:id", controller.UpdateAStory)
+		v1Group.PUT("/api/readme/:id", controller.UpdateAStory)
 		//删除我的某个故事
-		v1Group.DELETE("/readme/:id", controller.DeleteAStory)
+		v1Group.DELETE("/api/readme/:id", controller.DeleteAStory)
 	}
 	return r
 }
